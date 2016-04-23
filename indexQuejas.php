c7e280cddf7cfd6d9dca6834f8c67d77118633b5
crisus <cristian.canseco@cartum.org>
2016-04-23 19:02:08 +0200

    cabecera pagina

--- indexQuejas.php
+++ indexQuejas.php
@@ -26,8 +26,33 @@
 		<script src="http://html5shiv.google.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 	</head>
-
 	<body>
+			<header id="cabecera">
+			<img class="logo" src="<?php echo $ruta;?>img/casaasturiasescudo.png"></img>
+			<h1>Casa De Asturias En Leon</h1>
+			<img class="logo" src="<?php echo $ruta;?>img/casaasturiasescudo.png"></img>
+		</header>
+
+		<nav id="menu">
+			<div class="identificacion">
+				USUARIO:
+<?php
+	if ($_SESSION['tipoUsuario'] == 1) {
+		echo 'SOCIO '.$_SESSION['nUsuario'];
+	} else if ($_SESSION['tipoUsuario'] == 2) {
+		echo 'EMPLEADO '.$_SESSION['nUsuario'];
+	}
+?>
+			</div>
+			<div class="menu-zona">
+				<ul class = "lista_menu">
+					<li><a href="<?php echo $ruta;?>server.php?inicio=<?php echo $_SESSION['indice'];?>">Inicio</a> </li>
+					<li><a href="<?php echo $ruta;?>index.html">Cambio de Usuario</a></li>
+				</ul>
+			</div>
+		</nav>
+
+
 
 		<footer id="pie">
 			<p>© ISW 2 | 2015-2016 | Cristian Canseco Blanco </p>
