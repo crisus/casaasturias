feb08836fb68eeef73d50b732412b9db2efcf5f3
crisus <cristian.canseco@cartum.org>
2016-04-23 18:48:42 +0200

    escuchando post

---
+++ server_quejas.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+	include_once "connection.inc";
+	include_once "sesiones.inc";
+	$ruta = '/casaasturias/';
+	comprobarVidaSesion();
+
+	//var mensaje="accion=reservar&nUsuario="+entrada.value+"&deporte="+deporte+"&pista="+pista+"&nbTiempo="+nbTiempo;
+	if ($_POST) {
+		//echo 'CONECTANDO';
+		$enlace = enlazarBBDD();
+		//echo 'CONECTADO';
+
+		$accion = $_POST['accion'];
+
+
+		mysqli_close($enlace);
+	}
+?>
