274ca9fb46dfe6fff9aa0ccf385b9a68eb211962
crisus <cristian.canseco@cartum.org>
2016-05-04 15:50:44 +0200

    html para presentar quejas

---
+++ indexModQuejas.php
@@ -0,0 +1,78 @@
+<?php
+	session_start();
+    include_once "connection.inc";
+	include_once "sesiones.inc";
+	$ruta = '/casaasturias/';
+	comprobarVidaSesion();
+
+	if ( ($_SESSION['nUsuario']) && ($_GET) ) {
+		$deporte = $_GET['v1'];
+		$pista = (int) $_GET['v2'];
+		$enlace = enlazarBBDD();
+	} else {
+		header("Location: ".$ruta."index.html");
+	}
+
+?>
+<!DOCTYPE html>
+<html lang="ES">
+	<head>
+		<title> SOCIEDAD REAL CASA DE ASTURIAS</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo $ruta;?>css/estilo.css">
+		<script type="text/javascript" src="<?php echo $ruta;?>js/comunicacion_quejas.js"></script>
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.google.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<header id="cabecera">
+			<img class="logo" src="<?php echo $ruta;?>img/casaasturiasescudo.png"></img>
+			<h1>Casa De Asturias En Leon</h1>
+			<img class="logo" src="<?php echo $ruta;?>img/casaasturiasescudo.png"></img>
+		</header>
+
+		<nav id="menu">
+			<div class="identificacion">
+				USUARIO:
+<?php
+				if ($_SESSION['tipoUsuario'] == 1) {
+					echo 'SOCIO '.$_SESSION['nUsuario'];
+				} else if ($_SESSION['tipoUsuario'] == 2) {
+					echo 'EMPLEADO '.$_SESSION['nUsuario'];
+				}
+?>
+			</div>
+			<div class="menu-zona">
+				<ul class = "lista_menu">
+					<li><a href="<?php echo $ruta;?>server.php?inicio=<?php echo $_SESSION['indice'];?>">Inicio</a> </li>
+					<li><a href="<?php echo $ruta;?>index.html">Cambio de Usuario</a></li>
+				</ul>
+			</div>
+		</nav>
+
+		<section id="contenido">
+			<div>
+				<h2>RESOLVER OBSERVACIONES</h2>
+				<form>
+					<input type="checkbox" class="verQuejas" value="REALIZADAS" onclick="obtenerQuejas()">
+					<input type="checkbox" class="verQuejas" value="ARCHIVADAS" onclick="obtenerQuejas()">
+					<input type="checkbox" class="verQuejas" value="SIN LEER" checked onclick="obtenerQuejas()">
+				</form>
+			</div>
+			<div class="observaciones">
+				<form>
+					<input type="text" id="observacion" class="queja">
+					<input type="text" id="asunto" class="asunto">
+					<button type="button" onclick="" class="accion"></button>
+				</form>
+			</div>
+		</section>
+
+
+		<footer id="pie">
+			<p>© ISW 2 | 2015-2016 | Cristian Canseco Blanco </p>
+		</footer>
+
+	</body>
+</html>
