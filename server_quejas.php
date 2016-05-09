b45f0859430f62287d187614cb5531bb8674af8e
crisus <cristian.canseco@cartum.org>
2016-05-09 16:49:18 +0200

    respuesta con quejas para mostrar

--- server_quejas.php
+++ server_quejas.php
@@ -23,6 +23,20 @@
 				} else {
 					echo "_ERROR_1_";
 				}
+		} else if (accion=='verQuejas') {
+			$realizadas = $_POST['re'];
+			$archivadas = $_POST['ar'];
+			$sinLeer = $_POST['nole'];
+			$data = getQuejas($realizadas, $archivadas, $sinLeer);
+			if ($data !== '') {
+				echo '_OK_2_'.$data;
+			} else {
+				echo '_ERROR_2_';
+			}
+		} else if (accion=='archivar') {
+			$id = $_POST['objeto'];
+		} else if (accion=='agregar') {
+			$id = $_POST['objeto'];
 		} else {
 			echo "_ERROR_2_";
 		}
