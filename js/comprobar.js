92b62a46fa92021dce110f565014940c24d895c2
crisus <cristian.canseco@cartum.org>
2016-05-11 10:28:35 +0200

    refactorizacion tratar_observaciones -- comprobar

--- js/comprobar.js
+++ js/comprobar.js
@@ -1,3 +1,11 @@
+function generarObservacion(entrada) {
+	var ruta = '/casaasturias/';
+    var pistaSeleccionada = document.getElementById(entrada);
+    var data =[];
+    data = entrada.split('_');
+	window.location=ruta+'indexQuejas.php?v1='+data[2]+'&v2='+data[3];
+}
+
 function reservarPista(entrada) {
 	var ruta = '/casaasturias/';
 	var data=[];
