ab14c30964d613710e20977c78657f232bab791d
crisus <cristian.canseco@cartum.org>
2016-05-25 10:28:26 +0200

    tareas continuas, finalizado

--- js/comprobar.js
+++ js/comprobar.js
@@ -56,7 +56,7 @@ function repetir(id, deporte, pista, id_tarea, realizarRepeticion) {
 			}
 			if (diasReserva !== '') {
 				mensaje = mensaje +diasReserva+'&fechaI='+fets[0].value+'&fechaF='+fets[1].value;
-				alert(mensaje);
+				//alert(mensaje);
 				comunicacion(id,mensaje);
 			} else {
 				alert('No hay repeticiones señaladas');
@@ -177,7 +177,7 @@ function recibir(id,salida) {
 			out.value = er[3];
 			entrada.disabled = true;
 		} else if (er[2] == '6') { // tarea con continuidad (semanal)
-			alert('tarea semanal'+er[3]);
+			alert('TAREA CONTINUA');
 		}
 		window.location.reload();
 	} else if (er[1] == 'ERROR') {
@@ -208,10 +208,10 @@ function recibir(id,salida) {
 			alert("Sesion sigue activa");
 		}
 	} else {
-		alert("DESCONOCIDO _"+salida+"_");
+		//alert("DESCONOCIDO _"+salida+"_");
 		//window.location.reload();
 
-		//alert("LA SESION DEL SERVIDOR\n HABIA FINALIZADO");
+		alert("LA SESION DEL SERVIDOR\n HABIA FINALIZADO");
 		window.location=ruta+"index.html";
 	}
 }
