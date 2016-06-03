375bd60e44489d75feb1143a2a2bd70748669f58
crisus <cristian.canseco@cartum.org>
2016-06-03 11:46:11 +0200

    generador claves

--- encriptador.php
+++ encriptador.php
@@ -4,16 +4,28 @@
 	if ($_POST) {
 		$accion=$_POST['accion'];
 		if ( $accion == 'gamal') {
-			$clavePublica = getKey();
-			echo "_OK_".$GLOBALS['key_public']."_".$GLOBALS['key_private']."_";
+			getKey();
+			$clavePublica = $GLOBALS['key_public'];
+			echo "_OK_g:".$clavePublica[0]." p:".$clavePublica[1]." k:".$clavePublica[2]."_".$GLOBALS['key_private']."_";
 		} else {
 			echo "_ERROR_000_";
 		}
 	}
 
 	function getKey() {
-		$a = generarPrimos(10);
-		$GLOBALS['key_public'] = 10;
+		$p = generarPrimos(100);
+		$a = gmp_random_range(0,$p-1);
+		$g = gmp_random_range(0,$p-1);
+		$k = ExpModP($g, $a, $p);
+		$GLOBALS['key_public'] = [$g,$p,$k];
 		$GLOBALS['key_private'] = $a;
+	}
+
+	function ExpModP($g,$a,$p){
+		$k=$g;
+		for ($i=0; $i<$p; $i++) {
+			$k = ($k * $k)%$p;
+		}
+		return $k;
 	}
 ?>
